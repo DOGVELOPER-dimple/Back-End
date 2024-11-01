63b7326125cc42af1918c059fb01c366b29ecfab
sdrong <aaa957aaa@naver.com>
2024-11-01 17:02:40 +0900

    init

---
+++ src/test/java/dogveloper/vojoge/VojogeApplicationTests.java
@@ -0,0 +1,13 @@
+package dogveloper.vojoge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VojogeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
