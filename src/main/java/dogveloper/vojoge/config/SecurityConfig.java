b4e5474b2edf3e03a9fcd11786c7019c7898e400
Raymond Yoon <yoonjunsang@naver.com>
2024-11-11 15:45:36 +0900

    jwt없이 구글로그인 구현

--- src/main/java/dogveloper/vojoge/config/SecurityConfig.java
+++ src/main/java/dogveloper/vojoge/config/SecurityConfig.java
@@ -1,78 +1,36 @@
 package dogveloper.vojoge.config;
 
-import dogveloper.vojoge.social.handler.OAuth2LoginSuccessHandler;
-import dogveloper.vojoge.jwt.JwtAuthenticationFilter;
-import dogveloper.vojoge.social.service.CustomOAuth2UserService;
+import dogveloper.vojoge.user.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
     private final CustomOAuth2UserService customOAuth2UserService;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfig(OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler,
-                          CustomOAuth2UserService customOAuth2UserService,
-                          JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.oAuth2LoginSuccessHandler = oAuth2LoginSuccessHandler;
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
         this.customOAuth2UserService = customOAuth2UserService;
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(request -> {
-                    var config = new CorsConfiguration();
-                    config.setAllowedOrigins(List.of(
-                            "http://localhost:3000",
-                            "http://10.0.2.2:8080",
-                            "https://4dce-222-118-182-61.ngrok-free.app",
-                            "https://1df2-203-237-200-56.ngrok-free.app"));
-                    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
-                    config.setAllowedHeaders(List.of("*"));
-                    config.setAllowCredentials(true);
-                    return config;
-                }))
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/login/**", "/auth/logout", "/oauth2/**").permitAll()
-                        .requestMatchers("/auth/protected").authenticated()
+                .csrf(csrf -> csrf.disable()) // CSRF 설정을 비활성화합니다.
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/login/**", "/oauth2/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .oauth2Login(oauth -> oauth
-                        .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .successHandler(oAuth2LoginSuccessHandler)
-                        .failureUrl("/auth/login/failure")
-                )
-                .headers(headers -> headers
-                        .addHeaderWriter((request, response) -> {
-                            response.addHeader("Content-Security-Policy", "default-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; " +
-                                    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-                                    "font-src 'self' https://fonts.gstatic.com; " +
-                                    "script-src 'self' 'unsafe-inline'; " +
-                                    "connect-src 'self';");
-                        })
-                )
-        ;
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                );
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
