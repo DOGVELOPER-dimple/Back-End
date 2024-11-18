c804525ea50375b5c69551761ee8bca62f39a76e
Raymond Yoon <yoonjunsang@naver.com>
2024-11-18 23:31:28 +0900

    jwt 토큰 provider 추가

--- src/main/java/dogveloper/vojoge/user/CustomOAuth2UserService.java
+++ src/main/java/dogveloper/vojoge/user/CustomOAuth2UserService.java
@@ -1,5 +1,6 @@
 package dogveloper.vojoge.user;
 
+import dogveloper.vojoge.jwt.JwtTokenProvider;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -13,10 +14,12 @@ import java.util.Optional;
 public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
     private final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
 
-    public CustomOAuth2UserService(UserRepository userRepository) {
+    public CustomOAuth2UserService(UserRepository userRepository, JwtTokenProvider jwtTokenProvider) {
         this.userRepository = userRepository;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
@@ -46,6 +49,12 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
             userRepository.save(user);
         }
 
-        return oAuth2User;
+        // JWT 토큰 생성
+        String jwtToken = jwtTokenProvider.generateToken(email);
+
+        // JWT 토큰을 반환할 수 있도록 설정
+        // 예를 들어, 이 값을 `OAuth2User` 객체의 속성으로 설정하거나 필요한 곳에서 사용
+
+        return oAuth2User;  // 실제로는 JWT를 응답으로 반환하거나 세션에 저장하는 등의 추가 작업 필요
     }
 }
