7e2a0565d4646f7c02ca3aafd7adad77086fb626
Raymond Yoon <yoonjunsang@naver.com>
2025-01-22 18:36:17 +0900

    스웨거 토큰 추가

--- src/main/java/dogveloper/vojoge/notification/dto/NotificationRequest.java
+++ src/main/java/dogveloper/vojoge/notification/dto/NotificationRequest.java
@@ -1,11 +1,14 @@
 package dogveloper.vojoge.notification.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class NotificationRequest {
+    @Schema(hidden = true)
     private Long userId;
+
     private Long dogId;
     private String message;
     private LocalDateTime scheduledTime;
